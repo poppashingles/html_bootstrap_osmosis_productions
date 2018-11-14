f30c1fa66f819883107aed978ac9c4ead4288f3b
Rob <robdwilliams85@gmail.com>
2018-11-14 23:28:20 +0000

    Lightbox

--- js/fizzfunction.js
+++ js/fizzfunction.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 			}).on('mouseout', function(){
 				$('.dropdown-toggle', this).next('.dropdown-menu').hide();
 			});
-			
+
 			$('.dropdown-toggle').click(function() {
 				if ($(this).next('.dropdown-menu').is(':visible')) {
 					window.location = $(this).attr('href');
@@ -17,10 +17,10 @@ $(document).ready(function() {
 			$('.navbar-default .dropdown').off('mouseover').off('mouseout');
 		}
 	}
-	
+
 	$(window).resize(function() {
 		bindNavbar();
 	});
-	
+
 	bindNavbar();
 });
